d623ae4f5aa92e610a5f6f0844c753d5cf9c4572
amd5500gt <jasss.pb03@gmail.com>
2026-05-06 05:09:11 +0530

    updated

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-require("../Models/db");
 
 const cors = require("cors");
 const bodyparser = require("body-parser");
