68f834dab4caf436e88f386ab95f0033271c1fde
amd5500gt <jasss.pb03@gmail.com>
2026-05-05 15:24:47 +0530

    fix gitignore

--- BackEnd/Models/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose")
-
-const mongoURL = process.env.mongodb_URL
-
-mongoose.connect(mongoURL)
-.then(()=> console.log("Database Connected...."))
-.catch((err)=> console.log("Connect failed.... ",err))
--- BackEnd/Routers/productRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express").Router()
-
-const productsAPIs = require("../Controllors/productAPIs")
-const ensureAuthenticated = require("../Middlewares/authData")
-
-Router.get("/products",ensureAuthenticated, productsAPIs )
-
-
-module.exports = Router
