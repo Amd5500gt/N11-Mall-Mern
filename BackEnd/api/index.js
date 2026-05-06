3e9af0564d088d0c2e3323f6c390ac5d84a0319f
amd5500gt <jasss.pb03@gmail.com>
2026-05-06 11:33:21 +0530

    uped

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -1,5 +1,16 @@
+const serverless = require("serverless-http");
 const app = require("../index");
 
-module.exports = (req, res) => {
-  return app(req, res);
+module.exports = async (req, res) => {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // 🔥 preflight request fix
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  return serverless(app)(req, res);
 };
