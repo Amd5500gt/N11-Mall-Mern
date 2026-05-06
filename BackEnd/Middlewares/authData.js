bdaf3fb85bf88c4e392f1638c8b1874b6e252821
amd5500gt <jasss.pb03@gmail.com>
2026-05-06 13:49:34 +0530

    a

--- BackEnd/Middlewares/authData.js
+++ BackEnd/Middlewares/authData.js
@@ -1,28 +1,39 @@
 const jwt = require("jsonwebtoken");
-const demodata = require("../productData/demodata.json")
+
+const demodata = require("../productData/demodata.json");
+
 const ensureAuthenticated = (req, res, next) => {
-    const authHeader = req.headers.authorization;
 
-    try {
-        if (!authHeader) {
-              
-            return res.status(403).json(demodata)
+  const authHeader = req.headers.authorization;
 
-        
-        }
-        // ✅ correct split
-        const token = authHeader.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.jwt_secret);
-        req.user = decoded;
-        next();
-    } catch (err) {
-        if (!req.user) {
-        res.json(demodata);
-      }
-        res.status(401).json({
-            message: "Invalid or expired token"
-        });
+  try {
+
+    // NO TOKEN
+    if (!authHeader) {
+
+      return res.status(200).json(demodata);
+
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(
+      token,
+      process.env.jwt_secret
+    );
+
+    req.user = decoded;
+
+    next();
+
+  } catch (err) {
+
+    console.log(err);
+
+    return res.status(200).json(demodata);
+
   }
+
 };
 
 module.exports = ensureAuthenticated;
