bf489111422d1024cfb596c0ef347f9fee65ee07
amd5500gt <jasss.pb03@gmail.com>
2026-05-06 12:53:14 +0530

    a

--- BackEnd/Models/db.js
+++ BackEnd/Models/db.js
@@ -1,7 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const mongoURL = process.env.mongodb_url
+const mongoURL = process.env.mongodb_url;
 
-mongoose.connect(mongoURL)
-.then(()=> console.log("Database Connected...."))
-.catch((err)=> console.log("Connect failed.... ",err))
+async function connectDB() {
+
+  try {
+
+    await mongoose.connect(mongoURL);
+
+    console.log("MongoDB Connected");
+
+  } catch (err) {
+
+    console.log("Connect failed.... ", err);
+
+  }
+
+}
+
+module.exports = connectDB;
