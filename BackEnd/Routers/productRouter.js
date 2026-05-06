f88b77351407ea1454a305188340447b2d8d35d5
amd5500gt <jasss.pb03@gmail.com>
2026-05-06 15:27:58 +0530

    fixed routers in backend

--- BackEnd/Routers/productRouter.js
+++ BackEnd/Routers/productRouter.js
@@ -1,13 +1,13 @@
 const Router = require("express").Router()
 const {products, productPage} = require("../Controllors/productAPIs")
-const products = require("../productData/demodata.json")
+const demodata = require("../productData/demodata.json")
 const ensureAuthenticated = require("../Middlewares/authData")
 
 
-Router.get("/",ensureAuthenticated, products )
 Router.get("/demo", (req, res) => {
   res.json(demodata);
  });
+Router.get("/",ensureAuthenticated, products )
 Router.get("/:id", productPage );
 
 module.exports = Router
