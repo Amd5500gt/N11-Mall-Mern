4703348aa47628b728ff3d3a6e087eefee72d3ab
amd5500gt <jasss.pb03@gmail.com>
2026-05-06 22:25:06 +0530

    a

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -18,8 +18,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
       "http://localhost:5173",
-     "https://nexxcartvx.vercel.app",
-    "https://nexxcart.pages.dev"
+     "https://nexxcartvx.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
