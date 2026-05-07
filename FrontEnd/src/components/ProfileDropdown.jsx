ecae2af828c134e8c979aced4a47d1f4aef14f94
Harjeet Singh <jasss.pb03@gmail.com>
2026-05-07 09:39:51 +0530

    Upder

--- FrontEnd/src/components/ProfileDropdown.jsx
+++ FrontEnd/src/components/ProfileDropdown.jsx
@@ -1,65 +1,42 @@
 // components/ProfileDropdown.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiUser, FiLogOut, FiMapPin, FiPackage } from 'react-icons/fi';
-import { FaUserCircle, FaMoon, FaSun } from 'react-icons/fa';
+import { FiUser, FiMapPin, FiPackage, FiLogOut, FiSettings } from 'react-icons/fi';
+import { Link } from 'react-router-dom'; // if using Link, otherwise use navigate
 
-const ProfileDropdown = ({ 
-  dropdownRef, 
-  userName, 
-  userEmail, 
-  userData, 
+const ProfileDropdown = ({
+  dropdownRef,
+  userName,
+  userEmail,
+  userData,
   addressesCount,
   onClose,
   onShowAddress,
+  onShowOrders,   // <-- new prop
   onLogout
 }) => {
   return (
-    <div ref={dropdownRef} className="profile-dropdown">
-      {/* User Info Header */}
+    <div className="profile-dropdown" ref={dropdownRef}>
       <div className="dropdown-header">
-        {userData?.avatar ? (
-          <img src={userData.avatar} alt="Profile" className="dropdown-avatar" />
-        ) : (
-          <FaUserCircle size={40} />
-        )}
         <div className="dropdown-user-info">
-          <h4>{userName || 'User'}</h4>
-          <p>{userEmail || ''}</p>
+          <strong>{userName}</strong>
+          <span>{userEmail}</span>
         </div>
       </div>
       <div className="dropdown-divider"></div>
-
-      {/* My Profile */}
-      <Link
-        to="/profile"
-        className="dropdown-item"
-        onClick={onClose}
-      >
-        <FiUser size={16} />
-        <span>My Profile</span>
-      </Link>
-
-
-      {/* Address Management */}
-      {/* <button className="dropdown-item" onClick={onShowAddress}>
-        <FiMapPin size={16} />
-        <span>Manage Address</span>
-        {addressesCount > 0 && <span className="badge">{addressesCount}</span>}
-      </button> */}
-
-      
-
-      <div className="dropdown-divider"></div>
-
-      {/* Logout */}
-      <button 
-        className="dropdown-item logout" 
-        onClick={onLogout}
-      >
-        <FiLogOut size={16} />
-        <span>Logout</span>
-      </button>
+      <ul className="dropdown-menu-list">
+        <li onClick={() => { onClose(); /* navigate to profile if needed */ }}>
+          <FiUser /> My Profile
+        </li>
+        <li onClick={() => { onShowAddress(); onClose(); }}>
+          <FiMapPin /> My Addresses
+        </li>
+        <li onClick={() => { onShowOrders(); onClose(); }}>   {/* new item */}
+          <FiPackage /> My Orders
+        </li>
+        <li onClick={onLogout}>
+          <FiLogOut /> Logout
+        </li>
+      </ul>
     </div>
   );
 };
