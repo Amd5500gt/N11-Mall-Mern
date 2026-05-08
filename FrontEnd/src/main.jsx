06e97aa7deecb07a088d706d35db7c22ccd6bc92
amd5500gt <jasss.pb03@gmail.com>
2026-05-08 16:03:17 +0530

    heheh fixed

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { CartProvider } from './context/CartContext'  // Change Cart to CartProvider
-import { SearchProvider } from './context/SearchContext'  // Change Cartcontext to SearchProvider
+import { CartProvider } from './context/CartContext.jsx'  // Change Cart to CartProvider
+import { SearchProvider } from './context/SearchContext.jsx'  // Change Cartcontext to SearchProvider
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
