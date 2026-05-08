6ef36aac9e8f9df6e491f5d8a25c53329d569456
amd5500gt <jasss.pb03@gmail.com>
2026-05-08 15:41:03 +0530

    update and fixed items from frontend

--- FrontEnd/src/Auth/components/GoogleAuth.jsx
+++ FrontEnd/src/Auth/components/GoogleAuth.jsx
@@ -1,14 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { LiaSpinnerSolid } from "react-icons/lia"
 
-const GoogleAuth = ({authMode}) => {
+const GoogleAuth = ({ authMode }) => {
+
+  const [googleLoaded, setGoogleLoaded] = useState(false);
+
+  useEffect(() => {
+
+    const checkGoogleButton = setInterval(() => {
+
+      const btn =
+        document.getElementById("googleBtn");
+
+      if (
+        btn &&
+        btn.childNodes.length > 0
+      ) {
+
+        setGoogleLoaded(true);
+
+        clearInterval(checkGoogleButton);
+
+      }
+
+    }, 300);
+
+    return () => clearInterval(checkGoogleButton);
+
+  }, []);
+
+  if (
+    authMode !== "login" &&
+    authMode !== "register"
+  ) {
+    return null;
+  }
+
   return (
-    <div>   {(authMode === 'login' || authMode === 'register') && (
-          <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'center' }}>
-            <div id="googleBtn"></div>
-          </div>
-        )}
-    </div>
-  )
-}
 
-export default GoogleAuth
+    <div >
+
+      {!googleLoaded && (
+
+        <div className="google-loading"
+        style={{
+        marginTop: "40px",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}>
+
+          <LiaSpinnerSolid
+            className="spinner"
+            size={30}
+          />
+
+        </div>
+
+      )}
+
+      <div
+        id="googleBtn"
+        style={{
+          opacity: googleLoaded ? 1 : 0
+        }}
+      />
+
+    </div>
+
+  );
+
+};
+
+export default GoogleAuth;
